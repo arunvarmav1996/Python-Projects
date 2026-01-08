9b2a30f210bcd759cdbe50030e9993945a0409eb
arunvarmav1996 <85779926+arunvarmav1996@users.noreply.github.com>
2026-01-08 13:22:04 +0000

    2 files added

---
+++ Roll a dice.py
@@ -0,0 +1,4 @@
+x = input("Enter the number of decimal points: ")
+y = 22/7
+print(round(y,int(x))) # Using round() function
+#print(f"{y:.{x}}")  --> Using f-string formatting
---
+++ madlibs.py
@@ -0,0 +1,9 @@
+#youtuber = "A&A Vlogs"
+#print(f"Subscribe to {youtuber}")
+
+Adjective = input("Adjective: ")
+Verb = input("Verb: ")
+Famous_Person = input("Famous Person: ")
+madlib = f"Computer programming is so {Adjective} ! It makes me so excited all the time because I love to {Verb}. \
+Stay hydrated and jump like you are {Famous_Person} !"
+print(madlib)
